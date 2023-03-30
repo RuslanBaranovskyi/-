1f4ce195f4bcd6dcede6d984fa6c516bac96bc39
Ruslan <sabackas@yandex.ru>
2023-03-30 04:41:28 +0400

    add Task 09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -7,7 +7,9 @@
 // 85 ->  // 80 % 10 = 5
 
 int numberRandom = new Random().Next(10,100);
+int numberRandom2 = new Random().Next(10,100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {numberRandom}");
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {numberRandom2}");
 
 int firstDigit = numberRandom / 10;
 int secondDigit = numberRandom % 10;
@@ -21,7 +23,23 @@ int secondDigit = numberRandom % 10;
 // Console.WriteLine ($"Наибольшая цифра числа => {secondDigit}");
 // }
 
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine ($"Наибольшая цифра числа => {maxDigit}");
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"наибольшая цифра числа => {maxDigit}");
+
 
 //Math.Max(firstDigit, secondDigit);
+
+int maxDigit = MaxDigit(numberRandom);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+
+int maxDigit2 = MaxDigit(numberRandom2);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit2}");
+
+
+int MaxDigit (int numRandom)
+{
+    int firstDigit = numRandom / 10;
+    int secondDigit = numRandom % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return maxDigit;
+}
