e47a738b1391b01bb3c45a125d7d3caba4c91cfb
Ruslan <sabackas@yandex.ru>
2023-03-31 01:05:40 +0400

    add Task 14 Method

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -4,3 +4,22 @@
 // 14 -> нет
 // 46 -> нет
 // 161 -> да
+
+Console.WriteLine($"Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Введите первое число для деления:");
+int firstNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Введите второе число для деления:");
+int secondNum = int.Parse(Console.ReadLine());
+
+bool result = IsMultipleTwoNumbers(num, firstNum, secondNum);
+Console.WriteLine(result? "Число кратное данным числам":"Число не кратное данныи числам");
+
+bool IsMultipleTwoNumbers(int number, int firstNumber, int secondNumber)
+
+{
+    return number % firstNumber == 0 && number % secondNumber == 0;
+
+}
