2039bc83e754815c69c1549bea800e2292b1ceb9
Ruslan <sabackas@yandex.ru>
2023-04-11 19:26:44 +0400

    add Task 42

---
+++ Task42/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите целое число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Двоичное число от числа {userNumber}равно {DecToBin(userNumber)}");
+
+int DecToBin(int num)
+{
+    int result = 0;
+    int count = 1;
+
+    while (num > 0)
+    {
+        result = result + num % 2 * count;
+        count *= 10;
+        num /= 2;
+    }
+    return result;
+}
