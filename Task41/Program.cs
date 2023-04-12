b0b56e30212ac37ac9a7ff9cf6d5e78f740f8bab
Ruslan <sabackas@yandex.ru>
2023-04-13 03:16:34 +0400

    add Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+
+int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите число {i + 1}:");
+        arr[i] =  Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+
+}
+
+int CountPositiveNumbers( int [] arr)
+{
+    int positiveCount = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            positiveCount++;
+        }
+    }
+    return positiveCount;
+}
+
+
+Console.Write("Введите количество чисел: ");
+int NumbersAmount = Convert.ToInt32(Console.ReadLine());
+int [] array = CreateArray(NumbersAmount);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+
+int PositiveCount = CountPositiveNumbers(array);
+Console.WriteLine($"Количество чисел больше 0: {PositiveCount}");
+
+
+
+
