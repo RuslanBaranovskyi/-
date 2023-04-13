fe626df99986b92463b408c1b7b85b9b7b0e16db
Ruslan <sabackas@yandex.ru>
2023-04-13 13:11:08 +0400

    add Task 49

---
+++ Task49/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+Console.WriteLine("Введите количество строк таблицы");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов таблицы");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] resultArray = CreateMatrixRndInt(m, n, -10,10);
+PrintArray(resultArray);
+Console.WriteLine("Модифицированная матрица");
+
+ReplaceToSquares(resultArray);
+PrintArray(resultArray);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i < arr.Length - 1) Console.Write($"{arr[i, j],3} ");
+            else Console.Write($"{arr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void ReplaceToSquares(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i = i + 2)
+    {
+        for (int j = 0; j < arr.GetLength(1); j = j + 2)
+        {
+            arr[i, j] *= arr[i, j];
+        }
+        Console.WriteLine();
+    }
+}
