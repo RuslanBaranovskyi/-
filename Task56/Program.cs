4e17a880c757a3eb1988d74c45ae3491ad764986
Ruslan <sabackas@yandex.ru>
2023-04-21 21:59:21 +0400

    Task56

---
+++ Task56/Program.cs
@@ -0,0 +1,103 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+Console.WriteLine("Введите количество строк таблицы");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов таблицы");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] resultArray = CreateArrayMatrix(m, n, 1, 10);
+PrintArray(resultArray);
+int[] sum = SumRows(resultArray);
+
+Console.WriteLine($"Номер строки с наименьшей суммой элементов = {MinRowNumber(sum)}");
+
+
+
+int[,] CreateArrayMatrix(int rows, int colum, int min, int max)
+{
+    int[,] arr = new int[rows, colum];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i < arr.Length - 1) Console.Write($"{arr[i, j],3} ");
+            else Console.Write($"{arr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] SumRows(int[,] arr)
+{
+    int[] sum = new int[m];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+
+            sum[i] += arr[i, j];
+    }
+    return sum;
+}
+
+int MinRowNumber(int[] arr)
+{
+    int min = arr[0];
+    int index = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            index = i;
+        }   
+    }
+    return index;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
